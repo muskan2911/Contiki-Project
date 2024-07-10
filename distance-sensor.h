bd51bf3e66e2ac2bb6c15acf9510d067c552541c
muskan2911 <97096047+muskan2911@users.noreply.github.com>
2024-07-10 12:53:52 +0530

    Create distance-sensor.h

---
+++ distance-sensor.h
@@ -0,0 +1,12 @@
+#ifndef DISTANCE_SENSOR_H
+#define DISTANCE_SENSOR_H
+
+#include "lib/sensors.h"
+
+#define DISTANCE_SENSOR "Distance Sensor"
+
+extern const struct sensors_sensor distance_sensor;
+
+#define DISTANCE_SENSOR_VALUE 0
+
+#endif /* DISTANCE_SENSOR_H */
